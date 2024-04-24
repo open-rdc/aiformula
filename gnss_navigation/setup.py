bb1a620b3993a8cf1418376cd574c6aeec1813ca
MasakiFujiwara <fmasaki2020@gmail.com>
2024-04-24 10:23:17 +0900

    Fix entry points

--- gnss_navigation/setup.py
+++ gnss_navigation/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'hoge = gnss_navigation.hoge:main'
+            'gnss_path_publisher = gnss_navigation.gnss_path_publisher:main'
         ],
     },
 )
