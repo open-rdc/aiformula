bcc8028c4add2515252c38aa8159d0044791057b
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-05-01 18:54:52 +0900

    add publish follow

--- gnss_navigation/launch/gnss_path_publish.launch.py
+++ gnss_navigation/launch/gnss_path_publish.launch.py
@@ -1,9 +1,17 @@
 # import statements
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+          'publish_transform',
+          default_value='true',
+          description='Whether to publish static transform from odom to base_link'
+        ),
         Node(
             package='gnss_navigation',
             executable='gnss_path_publisher',
@@ -11,5 +19,19 @@ def generate_launch_description():
             parameters=[
                 {'file_path': '/home/ros2_ws/src/AIFormula_private/gnss_navigation/config/course_data/gazebo_shihou_course.csv'}
             ]
+        ),
+        Node(
+            package='gnss_navigation',
+            executable='gnss_path_follower',
+            name='gnss_path_follower',
+            output='screen'
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_pub_odom_to_base_link',
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link'],
+            condition=IfCondition(LaunchConfiguration('publish_transform'))
         )
     ])
+
