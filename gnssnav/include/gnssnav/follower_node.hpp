4564ea8b6dcdb434cf93e6972a4a0cf6d7e3fd2d
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-05-30 19:56:35 +0900

    update follower

--- gnssnav/include/gnssnav/follower_node.hpp
+++ gnssnav/include/gnssnav/follower_node.hpp
@@ -2,12 +2,31 @@
 
 #include "gnssnav/visibility_control.h"
 
+#include <proj.h>
+
+#include <cmath>
+
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+#include <std_msgs/msg/empty.hpp>
+#include <std_msgs/msg/bool.hpp>
 #include <rclcpp/rclcpp.hpp>
+
+// #include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h> 
 
 namespace gnssnav{
 
 class Follower : public rclcpp::Node{
 public:
+    Follower();
+
+    void loop(void);
+    int freq_;
+
     GNSSNAV_PUBLIC
     explicit Follower(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
@@ -15,7 +34,71 @@ public:
     explicit Follower(const std::string& name_space, const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
+    bool autonomous_flag_;
+    bool nav_start_flag_;
+    bool init_base_flag_;
 
+    double ld_;
+    double ld_x_;
+    double ld_y_;
+    double ld_min_;
+    double ld_gain_;
+    double v_;
+    double w_;
+    double v_max_;
+    double w_max_;
+    double cte_gain_;
+    double wheel_base_;
+    double distance_;
+    double theta;
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr vectornav_subscriber_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr nav_start_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr autonomous_flag_subscriber_;
+
+    rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr cmd_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr current_pose_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr current_ld_pub_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    std::vector<geometry_msgs::msg::PoseStamped> point_;
+    // std::vector<geometry_msgs::msg::Pose> front_wheel_pos;
+
+    void declareParameter(void);
+
+    void vectornavCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void navStartCallback(const std_msgs::msg::Empty::SharedPtr&);
+    void autonomousFlagCallback(const std_msgs::msg::Bool::SharedPtr msg);
+ 
+    double calculateCrossError();
+    double calculateHeadingError();
+
+    void findNearestIndex(const geometry_msgs::msg::Pose);
+    void publishCurrentPose(void);
+    void publishLookahead(void);
+    void followPath(const geometry_msgs::msg::PoseStamped pose_msg);
+    void findLookaheadDistance(const geometry_msgs::msg::PoseStamped pose_msg);
+    void setBasePose(void);
+    void calcPathDirection(void);
+
+    double calculateYawFromQuaternion(const geometry_msgs::msg::Quaternion&);
+    std::pair<double, double> convertECEFtoUTM(double x, double y, double z);
+    double radian2deg(double rad);
+
+    double current_position_x_;
+    double current_position_y_;
+    double current_yaw_;
+    size_t idx_;
+    size_t pre_point_idx;
+    double vectornav_base_x_;
+    double vectornav_base_y_;
+    double vectornav_base_yaw_;
+    double path_direction_;
+
+    geometry_msgs::msg::Point pre_point;
 };
 
 }  // namespace gnssnav
--- gnssnav/src/path_publisher_node.cpp
+++ gnssnav/src/path_publisher_node.cpp
@@ -2,5 +2,110 @@
 
 namespace gnssnav{
 
+Publisher::Publisher()
+    :Node("path_publisher_node"),
+    init_flag_(true)
+{
+    initCommunication();
+    loadCSV();
+    path_msg_ = setMsg(xs_, ys_);
+    origin_path_msg_ = setMsg(origin_xs_, origin_ys_);
+}
 
-}  // namespace gnssnav
+// pub, param config
+void Publisher::initCommunication(void){
+    publisher_ = this->create_publisher<nav_msgs::msg::Path>("gnss_path", 10);
+    origin_publisher_ = this->create_publisher<nav_msgs::msg::Path>("origin_gnss_path", 10);
+
+    this->get_parameter("loop_freq", freq_);
+    file_path_ = this->get_parameter("file_path").as_string();
+}
+
+// load CSV file
+void Publisher::loadCSV(void){
+    std::ifstream file(file_path_);
+
+    while(std::getline(file, line_)) {
+        std::stringstream ss(line_);
+        tokens_.clear();
+        while(std::getline(ss, cell_, ',')) {
+            tokens_.push_back(cell_);
+        }
+        double lat = std::stod(tokens_[0]);
+        double lon = std::stod(tokens_[1]);
+        auto [x, y] = convertGPStoUTM(lat, lon);
+
+        if(init_flag_) setInitPose(x, y);
+
+        xs_.push_back(x - base_x_);
+        ys_.push_back(y - base_y_);
+        origin_xs_.push_back(x);
+        origin_ys_.push_back(y);
+    }
+}
+
+// init pose
+void Publisher::setInitPose(double x, double y){
+    base_x_ = x;
+    base_y_ = y;
+    init_flag_ = false;
+}
+
+// path create
+nav_msgs::msg::Path Publisher::setMsg(const std::vector<double>& xs, const std::vector<double>& ys){
+    std::vector<Eigen::Vector2d> spline_points = interpolateSpline(xs, ys, 100);
+
+    nav_msgs::msg::Path path_msg;
+    path_msg.header.stamp = this->now();
+    path_msg.header.frame_id = 'map';
+    for (const auto& coord : spline_points) {
+        geometry_msgs::msg::PoseStamped pose;
+        pose.header.stamp = this->now();
+        pose.header.frame_id = "map";
+        pose.pose.position.x = coord.x();
+        pose.pose.position.y = coord.y();
+        path_msg.poses.push_back(pose);
+    }
+
+    return path_msg;
+}
+
+// spline
+std::vector<Eigen::Vector2d> Publisher::interpolateSpline(const std::vector<double>& xs, const std::vector<double>& ys, int num_points){
+    Eigen::Matrix<double, Eigen::Dynamic, 2> points(xs.size(), 2);
+    for (size_t i=0; i < xs.size(); ++i){
+        points(i, 0) = xs[i];
+        points(i, 1) = ys[i];
+    }
+
+    auto spline = Eigen::SplineFitting<Eigen::Spline<double, 2>>::Interpolate(points.transpose(), 2); //2次のキュービックスプライン
+
+    double step = 1.0 / (num_points -1);
+    for (int i = 0; i < num_points; ++i) {
+        double u = i * step;
+        Eigen::Vector2d pt = spline(u);
+        result_.push_back(pt);
+    }
+    return result_;
+}
+
+// WGS84系からUTM座標系へ変換
+std::pair<double, double> Publisher::convertGPStoUTM(double lat, double lon) {
+    if (!(-90 <= lat) || !(lat <= 90) || !(-180 <= lon) || !(lon <= 180)) {
+        std::cerr << "Error: Latitude or longitude values are out of valid range." << std::endl;
+        return {std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()};
+    }
+    PJ *P = proj_create_crs_to_crs(PJ_DEFAULT_CTX, "EPSG:4326", "EPSG:32654", nullptr);
+    PJ_COORD p = proj_coord(lat, lon, 0, 0);
+    p = proj_trans(P, PJ_FWD, p);
+    proj_destroy(P);
+    return {p.xy.x, p.xy.y};
+}
+
+void Publisher::loop(void){
+    publisher_->publish(path_msg_);
+    origin_publisher_->publish(origin_path_msg_);
+}
+
+
+};  // namespace gnssnav
