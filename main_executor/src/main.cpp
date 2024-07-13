2c6edfb7333c0b452c364619c1489d297e82a83c
KazumaFujino <c1000702@planet.kanazawa-it.ac.jp>
2024-07-13 19:49:56 +0900

    ✨feat:cybergear_if

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -3,6 +3,7 @@
 #include "socketcan_interface/socketcan_interface_node.hpp"
 #include "controller/controller_node.hpp"
 #include "roboteq_driver/roboteq_driver_node.hpp"
+#include "cybergear_interface/cybergear_interface_node.hpp"
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
@@ -15,10 +16,12 @@ int main(int argc, char * argv[]){
     auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto roboteq_driver_node = std::make_shared<roboteq_driver::RoboteqDriver>(nodes_option);
+    auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
 
     exec.add_node(socketcan_node);
     exec.add_node(controller_node);
-    exec.add_node(roboteq_driver_node);
+    // exec.add_node(roboteq_driver_node);
+    exec.add_node(cybergear_interface_node);
 
     exec.spin();
     rclcpp::shutdown();
