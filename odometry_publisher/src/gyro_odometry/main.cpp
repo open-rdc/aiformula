b9b49789b82c04ab0477071c5d5b2818aae606b4
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2024-08-20 18:38:00 +0900

    🐛fix:rm odometry pub

--- odometry_publisher/src/gyro_odometry/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "gyro_odometry.hpp"
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<aiformula::GyroOdometry>());
-    rclcpp::shutdown();
-    return 0;
-}
