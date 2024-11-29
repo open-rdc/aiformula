834bd20147e5d70f09b8a2de8355b91482a87c49
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-11-29 16:48:02 +0900

    ✨ feat : 鳥瞰図

--- path_planner/include/path_planner/path_planner_node.hpp
+++ path_planner/include/path_planner/path_planner_node.hpp
@@ -2,6 +2,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include "path_planner/visibility_control.h"
 
 namespace path_planner{
@@ -18,16 +19,29 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr line_image_sub_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
 
-  sensor_msgs::msg::Image image_;
+  cv_bridge::CvImagePtr cv_img;
+  // sensor_msgs::msg::Image image_;
 
   rclcpp::TimerBase::SharedPtr timer_;
 
-  void ImageCallback(const sensor_msgs::msg::Image img) {
-    image_ = img;
+  void ImageCallback(const sensor_msgs::msg::Image::SharedPtr img) {
+    cv_img = cv_bridge::toCvCopy(img, img->encoding);
   }
   void PathPlanning();
 
   const int interval_ms;
+  const std::vector<cv::Point2f> srcPoints = {
+    cv::Point2f(150, 1080),
+    cv::Point2f(760, 540),
+    cv::Point2f(1140, 540),
+    cv::Point2f(1650, 1080)
+  };
+  const std::vector<cv::Point2f> dstPoints = {
+    cv::Point2f(0, 1080),    
+    cv::Point2f(0, 0),
+    cv::Point2f(1920, 0),
+    cv::Point2f(1920, 1080)
+  };
 };
 
 }  // namespace path_planner
--- path_planner/src/path_planner_node.cpp
+++ path_planner/src/path_planner_node.cpp
@@ -8,7 +8,7 @@ PathPlannerNode::PathPlannerNode(const std::string& name_space, const rclcpp::No
 : rclcpp::Node("path_planner_node", name_space, options),
 interval_ms(get_parameter("interval_ms").as_int())
 {
-    line_image_sub_ = this->create_subscription<sensor_msgs::msg::Image>("/line_detections/image", 10, std::bind(&PathPlannerNode::ImageCallback, this, std::placeholders::_1));
+    line_image_sub_ = this->create_subscription<sensor_msgs::msg::Image>("/line_detection/image", 10, std::bind(&PathPlannerNode::ImageCallback, this, std::placeholders::_1));
     image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/upstream_image", 10);
 
     timer_ = this->create_wall_timer(
@@ -18,7 +18,17 @@ interval_ms(get_parameter("interval_ms").as_int())
 }
 
 void PathPlannerNode::PathPlanning(){
-    image_pub_->publish(image_);
+    if(cv_img->image.empty())   return;
+
+    cv::Mat transformMatrix = cv::getPerspectiveTransform(srcPoints, dstPoints);
+    cv::Mat birdView;
+
+    cv::warpPerspective(cv_img->image, birdView, transformMatrix, cv::Size(1920, 1080));
+
+    // image_pub_->publish(image_);
+    sensor_msgs::msg::Image::SharedPtr ros_img = cv_bridge::CvImage(cv_img->header, "bgr8", birdView).toImageMsg();
+
+    image_pub_->publish(*ros_img);
 }
 
 }  // namespace path_planner
