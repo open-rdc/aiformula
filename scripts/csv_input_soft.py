6879d8f278e21123da303708d924b846ed0c7491
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-01-17 23:57:52 +0900

    ✨feat: ECEFのcsvを作成

--- scripts/csv_input_soft.py
+++ scripts/csv_input_soft.py
@@ -1,28 +1,27 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import csv
 
 class Converter(Node):
     def __init__(self):
         super().__init__("gps_csv_node")
-        self.subscription = self.create_subscription(NavSatFix, "/vectornav/gnss", self.callback, 10)
+        self.subscription = self.create_subscription(PoseWithCovarianceStamped, "/vectornav/pose", self.callback, 10)
         self.subscription
-        self.get_logger().info("vectornav/gnssをcsvファイルにします。")
+        self.get_logger().info("vectornav/poseをcsvファイルにします。")
 
-        self.csv_file = open('shihou_gnssnav.csv', mode='w', newline='')
+        self.csv_file = open('default_name.csv', mode='w', newline='')
         self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(['Latitude', 'Longitude'])
-
         self.counter = 0  # カウンタを初期化
 
     def callback(self, msg):
         self.counter += 1  # コールバックが呼ばれるたびにカウンタをインクリメント
-        if self.counter % 10 == 0:  # 10回に1度だけ書き込み
-            latitude = msg.latitude
-            longitude = msg.longitude
-            print(latitude, longitude)
-            self.csv_writer.writerow([latitude, longitude])
+        if self.counter % 1000 == 0:  # 10回に1度だけ書き込み
+            latitude = msg.pose.pose.position.x
+            longitude = msg.pose.pose.position.y
+            ellipsoid = msg.pose.pose.position.z
+            print(latitude, longitude, ellipsoid)
+            self.csv_writer.writerow([latitude, longitude, ellipsoid])
             # self.get_logger().info(f'Latitude: {latitude}, Longitude: {longitude}')
 
     def destroy_node(self):
