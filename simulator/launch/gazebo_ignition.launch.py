0739fe854406e402fe083ef0c251dfd5ff768739
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-04-15 12:35:39 +0900

    ✨feat: ignition環境への移行（作業中）

---
+++ simulator/launch/gazebo_ignition.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import Node
+
+import os
+
+
+def generate_launch_description():
+    world_file_path = PathJoinSubstitution([
+        get_package_share_directory('simulator'),
+        'world',
+        'basic_world.sdf'
+    ])
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
+            '/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+            launch_arguments=[
+                ('gz_args', world_file_path)]
+        ),
+        bridge
+    ])
+
