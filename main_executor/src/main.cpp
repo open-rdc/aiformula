9fc2301b5fae409983ab0dd966d4f9b3e0576d3c
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-05-28 17:36:52 +0900

    🐛fix: バグ修正

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -4,8 +4,7 @@
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
 #include "cybergear_interface/cybergear_interface_node.hpp"
-#include "gnssnav/path_publisher_node.hpp"
-#include "gnssnav/follower_node.hpp"
+
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
@@ -28,8 +27,6 @@ int main(int argc, char * argv[]){
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
     auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
-    auto path_publisher_node = std::make_shared<gnssnav::Publisher>(nodes_option);
-    auto follower_node = std::make_shared<gnssnav::Follower>(nodes_option);
 
     if(sim_flag){}
     if(not sim_flag){
@@ -38,9 +35,7 @@ int main(int argc, char * argv[]){
     }
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
-    exec.add_node(cybergear_interface_node);
-    exec.add_node(path_publisher_node);
-    exec.add_node(follower_node);
+    // exec.add_node(cybergear_interface_node);
 
     exec.spin();
     rclcpp::shutdown();
