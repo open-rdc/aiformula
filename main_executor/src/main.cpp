aeef5e1ab9798a5d355407f9b48ab1553a1e04cf
rdclab <s24s1037hw@s.chibakoudai.jp>
2025-06-28 20:04:08 +0900

    🐛fix: 250626params

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -3,7 +3,6 @@
 #include "socketcan_interface/socketcan_interface_node.hpp"
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
-#include "cybergear_interface/cybergear_interface_node.hpp"
 #include "gnssnav/path_publisher_node.hpp"
 #include "gnssnav/follower_node.hpp"
 
@@ -26,7 +25,6 @@ int main(int argc, char * argv[]){
     auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
-    auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
     auto path_publisher_node = std::make_shared<gnssnav::Publisher>(nodes_option);
     auto follower_node = std::make_shared<gnssnav::Follower>(nodes_option);
 
@@ -36,7 +34,6 @@ int main(int argc, char * argv[]){
     }
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
-    exec.add_node(cybergear_interface_node);
     exec.add_node(path_publisher_node);
     exec.add_node(follower_node);
 
