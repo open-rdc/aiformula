f6d39128092d4817e437bdd5f859805cc188f0fc
ken222d <a.w.g.d0201@icloud.com>
2025-11-03 14:45:02 +0900

    Edit sdffile

--- simulator/launch/urdf_gazebo_ignition.launch.py
+++ simulator/launch/urdf_gazebo_ignition.launch.py
@@ -1,11 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
-
 from launch_ros.actions import Node
-
 import os
 
 
@@ -13,8 +11,41 @@ def generate_launch_description():
     world_file_path = PathJoinSubstitution([
         get_package_share_directory('simulator'),
         'world',
-        'shihou_world.sdf'
+        'shihou_urdf_world.sdf'
     ])
+
+    # === URDFファイルパス ===
+    urdf_file_path = os.path.join(
+        get_package_share_directory('simulator'),
+        'urdf',
+        'ai_car1.urdf'
+    )
+
+    set_env = SetEnvironmentVariable(name='IGN_GAZEBO_PHYSICS_COLLIDE_SAME_MODEL', value='1')
+
+    # === robot_state_publisher ===
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': open(urdf_file_path).read()
+        }]
+    )
+
+    # === URDFモデルをGazeboにスポーン ===
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'motor_spin_robot',
+            '-topic', 'robot_description',
+            '-x', '0', '-y', '0', '-z', '0.0'
+        ],
+        output='screen'
+    )
 
     # --- controller spawner ---
     joint_state_spawner = Node(
@@ -46,6 +77,7 @@ def generate_launch_description():
         output='screen'
     )
 
+    
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
@@ -53,8 +85,10 @@ def generate_launch_description():
             launch_arguments=[
                 ('gz_args', [world_file_path, ' -r'])]
         ),
+        set_env,
+        robot_state_publisher,
+        spawn_entity,   
         joint_state_spawner,
-        motor_controller_spawner,
+        motor_controller_spawne,
         bridge
     ])
-
