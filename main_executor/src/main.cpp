c145b195cb980102f8d472a3f6aa2ea5a8439fce
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2025-11-14 15:09:46 +0900

    🐛fix:no arg

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -1,6 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
 
-#include "socketcan_interface/socketcan_interface_node.hpp"
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
 
@@ -12,22 +11,10 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    bool sim_flag = false;
-    for(int i = 0; i < argc; ++i){
-        if(std::string(argv[i])=="--sim-flag"){
-            if(std::string(argv[i+1])=="true") sim_flag = true;
-            break;
-        }
-    }
 
-    auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
 
-    if(sim_flag){}
-    if(not sim_flag){
-        exec.add_node(socketcan_node);
-    }
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
 
