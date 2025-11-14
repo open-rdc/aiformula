001d5917a370e542536d67ff79d46ef2c8f08d5a
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2025-11-14 16:41:54 +0900

    ✨feat:pure pursuit

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
+#include "path_tracker/pure_pursuit_node.hpp"
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
@@ -14,9 +15,11 @@ int main(int argc, char * argv[]){
 
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
+    auto path_tracker_node = std::make_shared<path_tracker::PurePursuit>(nodes_option);
 
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
+    exec.add_node(path_tracker_node);
 
     exec.spin();
     rclcpp::shutdown();
