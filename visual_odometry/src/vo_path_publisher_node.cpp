09830b48cb4ff3741b2a48541d617499f1fb0c27
Keiyo Nakayama <Keiyou0206@gmail.com>
2025-12-01 16:55:49 +0900

    add a file

--- visual_odometry/src/vo_path_publisher_node.cpp
+++ visual_odometry/src/vo_path_publisher_node.cpp
@@ -1,1 +1,12 @@
 #include "visual_odmetry/vo_path_publisher_node.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+namespace visual_odometry
+{
+
+VoPathPublisher::VoPathPublisher(const rclcpp::NodeOptions & options)
+: rclcpp::Node("vo_path_publisher_node", options)
+{
+	this
