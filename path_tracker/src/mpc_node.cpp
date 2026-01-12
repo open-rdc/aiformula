613ce2556d3a2e58e182a214acfdb6f5187deda7
rdclab <s24s1037hw@s.chibakoudai.jp>
2026-01-12 18:57:42 +0900

    🐛fix: casadi config

--- path_tracker/src/mpc_node.cpp
+++ path_tracker/src/mpc_node.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cmath>
 #include <tf2/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace path_tracker {
 
