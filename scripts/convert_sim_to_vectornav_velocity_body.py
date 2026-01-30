87b334597801175368ec9c0ababab60adb70f6bf
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-01-30 18:30:04 +0900

    ✨feat: vectornav/velocity_bodyの出力再現

---
+++ scripts/convert_sim_to_vectornav_velocity_body.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from nav_msgs.msg import Odometry
+
+
+class Converter(Node):
+    def __init__(self):
+        super().__init__("sim_to_vectornav_velocity_body_converter_node")
+
+        self.create_subscription(Odometry, "/odom", self.callback_odom, 10)
+        self.publisher = self.create_publisher(TwistWithCovarianceStamped, "/vectornav/velocity_body", 10)
+
+        self.get_logger().info("/odom から /vectornav/velocity_body を再現して出版します．")
+
+    def callback_odom(self, msg: Odometry):
+        out = TwistWithCovarianceStamped()
+        out.header = msg.header
+
+        out.twist.twist = msg.twist.twist
+        out.twist.covariance = msg.twist.covariance
+
+        self.publisher.publish(out)
+
+
+def main():
+    rclpy.init()
+    node = Converter()
+
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
