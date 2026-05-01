09affbcd8185c8702b989e8b58764f0b36548420
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-05-01 20:00:41 +0900

    ✨feat: nav pkg統合

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -4,6 +4,11 @@
 #include "chassis_driver/chassis_driver_node.hpp"
 #include "frenet_planner/frenet_planner_node.hpp"
 #include "path_tracker/pure_pursuit_node.hpp"
+#include "vectormap_matching/map_odom_tf_node.hpp"
+#include "vectormap_matching/odom_tf_node.hpp"
+#include "vectormap_matching/vectormap_localization_node.hpp"
+#include "vectormap_planner/vectormap_planner_node.hpp"
+#include "vectormap_server/vectormap_server_node.hpp"
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
@@ -18,11 +23,23 @@ int main(int argc, char * argv[]){
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
     auto frenet_planner_node = std::make_shared<frenet_planner::FrenetPlannerNode>(nodes_option);
     auto path_tracker_node = std::make_shared<path_tracker::PurePursuit>(nodes_option);
+    auto vectormap_server_node = std::make_shared<vectormap_server::VectormapServerNode>(nodes_option);
+    auto vectormap_localization_node =
+        std::make_shared<vectormap_matching::VectormapLocalizationNode>(nodes_option);
+    auto odom_tf_node = std::make_shared<vectormap_matching::OdomTfNode>(nodes_option);
+    auto map_odom_tf_node = std::make_shared<vectormap_matching::MapOdomTfNode>(nodes_option);
+    auto vectormap_planner_node =
+        std::make_shared<vectormap_planner::VectormapPlannerNode>(nodes_option);
 
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
-    exec.add_node(frenet_planner_node);
-    exec.add_node(path_tracker_node);
+    // exec.add_node(frenet_planner_node);
+    // exec.add_node(path_tracker_node);
+    exec.add_node(vectormap_server_node);
+    exec.add_node(vectormap_localization_node);
+    exec.add_node(odom_tf_node);
+    exec.add_node(map_odom_tf_node);
+    exec.add_node(vectormap_planner_node);
 
     exec.spin();
     rclcpp::shutdown();
