7cea93ac568c9a41a03f7461acb534b4589bc237
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-05-03 20:55:39 +0900

    ✨feat: preception追加

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -1,12 +1,13 @@
 #include <rclcpp/rclcpp.hpp>
 
+#include "zed_wrapper/zed_wrapper_node.hpp"
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
 #include "frenet_planner/frenet_planner_node.hpp"
 #include "path_tracker/pure_pursuit_node.hpp"
-#include "vectormap_matching/map_odom_tf_node.hpp"
-#include "vectormap_matching/odom_tf_node.hpp"
-#include "vectormap_matching/vectormap_localization_node.hpp"
+#include "vectormap_localization/map_odom_tf_node.hpp"
+#include "vectormap_localization/odom_tf_node.hpp"
+#include "vectormap_localization/vectormap_localization_node.hpp"
 #include "vectormap_control/vectormap_controller_server.hpp"
 #include "vectormap_global_planner/vectormap_global_planner_node.hpp"
 #include "local_planner/local_planner_server.hpp"
@@ -20,6 +21,9 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
+    // Read launch flags from the 'launch' node's parameters
+    const bool use_zed =
+        rclcpp::Node("launch", nodes_option).get_parameter("zed").as_bool();
 
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
@@ -27,9 +31,9 @@ int main(int argc, char * argv[]){
     auto path_tracker_node = std::make_shared<path_tracker::PurePursuit>(nodes_option);
     auto vectormap_server_node = std::make_shared<vectormap_server::VectormapServerNode>(nodes_option);
     auto vectormap_localization_node =
-        std::make_shared<vectormap_matching::VectormapLocalizationNode>(nodes_option);
-    auto odom_tf_node = std::make_shared<vectormap_matching::OdomTfNode>(nodes_option);
-    auto map_odom_tf_node = std::make_shared<vectormap_matching::MapOdomTfNode>(nodes_option);
+        std::make_shared<vectormap_localization::VectormapLocalizationNode>(nodes_option);
+    auto odom_tf_node = std::make_shared<vectormap_localization::OdomTfNode>(nodes_option);
+    auto map_odom_tf_node = std::make_shared<vectormap_localization::MapOdomTfNode>(nodes_option);
     auto vectormap_global_planner_node =
         std::make_shared<vectormap_global_planner::VectormapGlobalPlannerNode>(nodes_option);
     auto local_planner_server_node =
@@ -37,6 +41,9 @@ int main(int argc, char * argv[]){
     auto vectormap_controller_server_node =
         std::make_shared<vectormap_control::VectormapControllerServer>(nodes_option);
 
+    if (use_zed) {
+        exec.add_node(std::make_shared<zed_wrapper::ZedWrapperNode>(nodes_option));
+    }
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
     // exec.add_node(frenet_planner_node);
