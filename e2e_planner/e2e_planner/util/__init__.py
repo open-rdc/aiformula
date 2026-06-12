422a39d94e225355bbec03a91aba9d2352044669
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-06-12 19:31:40 +0900

    🔥remove:　不要な機能・ファイルの削除

--- e2e_planner/e2e_planner/util/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Shared utilities for e2e_planner (sensor I/O, YOLOP, augmentation).
-
-Import submodules explicitly (e.g. ``from e2e_planner.util.sensor_utils import
-create_sensor_source``) so light-weight consumers do not pull heavy deps.
-"""
