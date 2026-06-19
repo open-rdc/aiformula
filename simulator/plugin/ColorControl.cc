e0f08aa9a728d6f5ada09dd7500553555b3ba70a
ken222d <a.w.g.d0201@icloud.com>
2026-06-20 00:30:31 +0900

    :bug:ignition系に変更

--- simulator/plugin/ColorControl.cc
+++ simulator/plugin/ColorControl.cc
@@ -29,35 +29,60 @@ void ColorControl::FindColorEntities(EntityComponentManager &_ecm)
 void ColorControl::PreUpdate(const UpdateInfo &_info,
                              EntityComponentManager &_ecm)
 {
-  if (_info.paused)
-    return;
-
-double simTimeInSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(_info.simTime).count();
-  this->FindColorEntities(_ecm);
+  // if (_info.paused)
+  //   return;
+  std::cout << "[ColorControl] PreUpdate Loop Pythonic Test!" << std::endl;
+  static uint64_t frameCount = 0;
+  frameCount++;
+  // double simTimeInSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(_info.simTime).count();
   if (this->ColorEntities.empty())
-    return;
-
+  {  
+    this->FindColorEntities(_ecm);
+    if (this->ColorEntities.empty())
+      return;
+  }
+  if (frameCount % 1000 == 0)
+  {
+    ignmsg << "Plugin is looping! Frame Count: " << frameCount 
+           << " | Gazebo SimTime: " << _info.simTime.count() << " ns\n";
+  }
   double r = 0.0;
   double g = 0.0;
   const double b = 0.0;
+  std::string color = "";
+  // if (std::abs(simTimeInSeconds - std::round(simTimeInSeconds)) < 0.005)
+  // {
+  //   ignmsg << "Current Sim Time: " << std::round(simTimeInSeconds) << "s | ";
+  //   if (static_cast<int>(simTimeInSeconds) % 2 == 0)
+  //   {
+  //     ignmsg << "Color set to RED\n";
+  //   }
+  //   else
+  //   {
+  //     ignmsg << "Color set to GREEN\n";
+  //   }
+  // }
 
-  if (static_cast<int>(simTimeInSeconds) % 2 == 0){
+  if ((frameCount / 2000) % 2 == 0){
     r = 1.0;
     g = 0.0;
+    color = "Red";
   }else{
     r = 0.0;
     g = 1.0;
+    color = "Green";
   } 
 
   ignition::math::Color newColor(r, g, b, 1.0);
 
   for (const Entity e : this->ColorEntities)
   {
+      std::cout << "Set" << color << "!!!!" << std::endl;
+
     auto ColorComp = _ecm.Component<components::Material>(e);
     if (!ColorComp)
       continue;
 
-    // ignition環境でも、この「Data()を直接書き換える方法」が一番きれいに動きます
     sdf::Material &sdfColor = ColorComp->Data();
 
     sdfColor.SetAmbient(newColor);
@@ -65,9 +90,9 @@ double simTimeInSeconds = std::chrono::duration_cast<std::chrono::duration<doubl
     sdfColor.SetEmissive(newColor);
 
     // データの変更を通知
-    _ecm.SetChanged(e,
-                    components::Material::typeId,
-                    ComponentState::PeriodicChange);
+     _ecm.SetChanged(e,
+                     components::Material::typeId,
+                     ComponentState::PeriodicChange);
   }
 }
 
