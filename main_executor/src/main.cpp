28b8b2efa24bc3dc3925948b97dc944c8f314b45
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-07-23 21:32:25 +0900

    ✨feat: localization構造を反映

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -3,9 +3,9 @@
 #include "zed_wrapper/zed_wrapper_node.hpp"
 #include "controller/controller_node.hpp"
 #include "chassis_driver/chassis_driver_node.hpp"
-#include "localization/map_odom_tf_node.hpp"
-#include "localization/odom_tf_node.hpp"
-#include "localization/localization_node.hpp"
+#include "ekf_localizer/odom_tf_node.hpp"
+#include "ekf_localizer/map_odom_tf_node.hpp"
+#include "pose_estimater/pose_estimater_node.hpp"
 #include "trajectory_follower/controller_server.hpp"
 #include "mission_planner/mission_planner_node.hpp"
 #include "local_planner/local_planner_server.hpp"
@@ -31,13 +31,13 @@ int main(int argc, char * argv[]){
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
     auto vectormap_server_node = std::make_shared<vectormap_server::VectormapServerNode>(nodes_option);
-    auto localization_node = std::make_shared<localization::LocalizationNode>(nodes_option);
-    auto odom_tf_node = std::make_shared<localization::OdomTfNode>(nodes_option);
-    auto map_odom_tf_node = std::make_shared<localization::MapOdomTfNode>(nodes_option);
+    auto odom_tf_node = std::make_shared<ekf_localizer::OdomTfNode>(nodes_option);
+    auto map_odom_tf_node = std::make_shared<ekf_localizer::MapOdomTfNode>(nodes_option);
+    auto pose_estimater_node = std::make_shared<pose_estimater::PoseEstimaterNode>(nodes_option);
     auto mission_planner_node = std::make_shared<mission_planner::MissionPlannerNode>(nodes_option);
     auto local_planner_server_node = std::make_shared<local_planner::LocalPlannerServer>(nodes_option);
     auto controller_server_node = std::make_shared<trajectory_follower::ControllerServer>(nodes_option);
-    auto object_detector_node = std::make_shared<object_detector::ObjectDetectorNode>(nodes_option);
+    // auto object_detector_node = std::make_shared<object_detector::ObjectDetectorNode>(nodes_option);
 
 #ifdef ENABLE_ZED
     std::shared_ptr<zed_wrapper::ZedWrapperNode> zed_wrapper_node;
@@ -49,13 +49,13 @@ int main(int argc, char * argv[]){
     exec.add_node(controller_node);
     exec.add_node(chassis_driver_node);
     exec.add_node(vectormap_server_node);
-    exec.add_node(localization_node);
     exec.add_node(odom_tf_node);
     exec.add_node(map_odom_tf_node);
+    exec.add_node(pose_estimater_node);
     exec.add_node(mission_planner_node);
     exec.add_node(local_planner_server_node);
     exec.add_node(controller_server_node);
-    exec.add_node(object_detector_node);
+    // exec.add_node(object_detector_node);
 
     exec.spin();
     rclcpp::shutdown();
