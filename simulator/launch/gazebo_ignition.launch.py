caf2d15ce48ecad75335f510e5df0da50a0ea6d2
ken222d <a.w.g.d0201@icloud.com>
2026-07-24 22:05:20 +0900

    🔧 chore: bridgeの追加

--- simulator/launch/gazebo_ignition.launch.py
+++ simulator/launch/gazebo_ignition.launch.py
@@ -35,7 +35,9 @@ def generate_launch_description():
             '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
             '/navsat@sensor_msgs/msg/NavSatFix@gz.msgs.NavSat',
             '/imu_raw@sensor_msgs/msg/Imu@gz.msgs.IMU',
-            '/cmd_vel_twist@geometry_msgs/msg/Twist@gz.msgs.Twist'],
+            '/cmd_vel_twist@geometry_msgs/msg/Twist@gz.msgs.Twist', 
+            '/obstacle/navsat@sensor_msgs/msg/NavSatFix@gz.msgs.NavSat',
+            '/obstacle/imu@sensor_msgs/msg/Imu@gz.msgs.IMU'],
         output='screen',
         remappings=[
             ('/image_raw', '/zed/zed_node/rgb/image_rect_color'),
