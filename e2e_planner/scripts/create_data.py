2c7e46cfad19a3d58b85bed929abcadc8762dad8
Kasaiatsuki <kasaiatsuki@gmail.com>
2026-09-04 15:36:26 +0900

    rgb画像を追加

--- e2e_planner/scripts/create_data.py
+++ e2e_planner/scripts/create_data.py
@@ -83,7 +83,7 @@ class DataCollectionNode(Node):
     def _initialize_zed_camera(self) -> None:
         self.zed_camera = sl.Camera()
         init_params = sl.InitParameters()
-        init_params.camera_resolution = sl.RESOLUTION.SVGA
+        init_params.camera_resolution = sl.RESOLUTION.HD720
         init_params.camera_fps = 30
         init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE
         init_params.coordinate_units = sl.UNIT.METER
